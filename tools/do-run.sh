e3139ebbf4d18a77cd1560d1a055f0c3146a1023
Christian Ledergerber <ledergec@gmail.com>
2018-11-23 19:29:19 +0100

    Get clion working properly in docker

--- tools/do-run.sh
+++ tools/do-run.sh
@@ -1,11 +1,22 @@
-if [ $# -eq 0 ]
-  then
-    echo "usage: do-run.sh path-to-rapidoson"
-    exit
-fi
+cd "$(dirname "$0")"
+
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+xhost + $IP
+
+socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+
+open -a XQuartz &
+
+docker rm rapidoson-dev-container
 
 docker run -it \
     --entrypoint /bin/bash \
+    -u root \
+    -e DISPLAY=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}'):0 \
     --name rapidoson-dev-container \
-    -v $1:/home/dev \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v `pwd`/.java:/root/.java \
+    -v ${HOME}/.Xauthority:/home/dev/.Xauthority \
+    -v `pwd`/.CLion2018.2:/root/.CLion2018.2 \
+    -v `pwd`/..:/home/dev/rapidoson \
     rapidoson-dev-image
