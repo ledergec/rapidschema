ec92082803a0895aea83a789199869231500f0a4
Christian Ledergerber <ledergec@gmail.com>
2018-11-24 09:00:03 +0100

    Add StringConstraintsTest

--- unittests/string_constraints_test.cpp
+++ unittests/string_constraints_test.cpp
@@ -1,55 +1,27 @@
-//
-// Created by root on 11/24/18.
-//
-
-#ifndef RAPIDOSON_STRING_CONSTRAINTS_TEST_H
-#define RAPIDOSON_STRING_CONSTRAINTS_TEST_H
-
 #include <gtest/gtest.h>
 
-#include "range_constraints.h"
+#include "string_constraints.h"
 #include "transform_result_matchers.h"
 
 namespace rapidoson {
 
-    TEST(RangeConstraints, GivenMinimum_WhenMinimumConstraintChecked_ThenSuccess) {
-        Minimum<int32_t, 3> minimum;
-        ASSERT_THAT(minimum.Check(3), CheckSucceeded());
+    TEST(StringConstraintsTest, GivenShortEnoughString_WhenMaxLengthConstraintChecked_ThenSuccess) {
+        MaxLength<3> max_length;
+        ASSERT_THAT(max_length.Check("mau"), CheckSucceeded());
     }
 
-    TEST(RangeConstraints, GivenJustOutOfBounds_WhenMinimumConstraintCheckt_ThenFails) {
-        Minimum<int32_t, 3> minimum;
-        ASSERT_THAT(minimum.Check(2), CheckFailed("Expected: >= 3. Actual: 2"));
+    TEST(StringConstraintsTest, GivenTooLongString_WhenMaxLengthConstraintChecked_ThenFails) {
+        MaxLength<3> max_length;
+        ASSERT_THAT(max_length.Check("miau"), CheckFailed("Expected std::string of length at most 3. Actual: miau of size 4"));
     }
 
-    TEST(RangeConstraints, GivenLargerThanMinimum_WhenExclusiveMinimumConstraintChecked_ThenFails) {
-        ExclusiveMinimum<int32_t, 3> exclusive_minimum;
-        ASSERT_THAT(exclusive_minimum.Check(4), CheckSucceeded());
+    TEST(StringConstraintsTest, GivenLongEnoughString_WhenMinLengthConstraintChecked_ThenSuccess) {
+        MinLength<3> min_length;
+        ASSERT_THAT(min_length.Check("mau"), CheckSucceeded());
     }
 
-    TEST(RangeConstraints, GivenMinimum_WhenExclusiveMinimumConstraintChecked_ThenSuccess) {
-        ExclusiveMinimum<int32_t, 3> exclusive_minimum;
-        ASSERT_THAT(exclusive_minimum.Check(3), CheckFailed("Expected: > 3. Actual: 3"));
-    }
-
-    TEST(RangeConstraints, GivenMaximum_WhenMaximumConstraintChecked_ThenSuccess) {
-        Maximum<int32_t, 3> maximum;
-        ASSERT_THAT(maximum.Check(3), CheckSucceeded());
-    }
-
-    TEST(RangeConstraints, GivenJustOutOfBounds_WhenMaximumConstraintChecked_ThenFails) {
-        Maximum<int32_t, 3> maximum;
-        ASSERT_THAT(maximum.Check(4), CheckFailed("Expected: <= 3. Actual: 4"));
-    }
-
-    TEST(RangeConstraints, GivenLowerThanMaximum_WhenMaximumConstraintChecked_ThenSuccess) {
-        ExclusiveMaximum<int32_t, 3> exclusive_maximum;
-        ASSERT_THAT(exclusive_maximum.Check(2), CheckSucceeded());
-    }
-
-    TEST(RangeConstraints, GivenMaximum_WhenMaximumConstraintChecked_ThenFails) {
-        ExclusiveMaximum<int32_t, 3> exclusive_maximum;
-        ASSERT_THAT(exclusive_maximum.Check(3), CheckFailed("Expected: < 3. Actual: 3"));
+    TEST(StringConstraintsTest, GivenTooShortString_WhenMinLengthConstraintChecked_ThenFails) {
+        MinLength<3> min_length;
+        ASSERT_THAT(min_length.Check("ma"), CheckFailed("Expected std::string of length at least 3. Actual: ma of size 2"));
     }
 }
-#endif //RAPIDOSON_STRING_CONSTRAINTS_TEST_H
