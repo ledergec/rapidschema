af2f14ac8a4929351d2a963bb6fce02656dbb3c8
Christian Ledergerber <ledergec@gmail.com>
2019-02-15 18:06:54 +0100

    Add OneOf

---
+++ include/rapidschema/one_of.h
@@ -0,0 +1,142 @@
+// Copyright (C) 2019 Christian Ledergerber
+
+#ifndef INCLUDE_RAPIDSCHEMA_VARIANT_H_
+#define INCLUDE_RAPIDSCHEMA_VARIANT_H_
+
+#include <assert.h>
+#include <functional>
+#include <string>
+#include <tuple>
+#include <utility>
+
+#include "rapidschema/concepts/correct_value_parameters.h"
+#include "rapidschema/concepts/requires_macro.h"
+#include "rapidschema/concepts/unique_json_types.h"
+#include "rapidschema/meta/json_type_set.h"
+#include "rapidschema/meta/unique_tuple.h"
+#include "rapidschema/rapidjson_type_to_string.h"
+#include "rapidschema/value.h"
+
+namespace rapidschema {
+
+namespace internal {
+
+template<typename Config>
+struct ConfigHasSameType {
+  template<typename OtherConfig>
+  using Condition = std::is_same<Config, OtherConfig>;
+};
+
+}  // namespace internal
+
+template<typename Encoding, typename... Configs> RAPIDSCHEMA_REQUIRES(UniqueTypes<typename Configs...>)
+class GenericOneOf;
+
+template<typename... Configs> RAPIDSCHEMA_REQUIRES(UniqueTypes<typename Configs...>)
+GenericOneOf<char, Configs...> MakeOneOf(Configs&&... configs);
+
+template<typename Ch, typename... Configs> RAPIDSCHEMA_REQUIRES(UniqueTypes<typename Configs...>)
+class GenericOneOf : public GenericConfig<Ch> {
+ private:
+  using Tuple = internal::UniqueTuple<Configs...>;
+
+  static constexpr int INVALID_ONE_OF_INDEX = -1;
+
+ public:
+  GenericOneOf() = default;
+
+  Result Transform(const rapidjson::Value& document) override {
+    size_t n_matches = 0;
+    Result result;
+    for (size_t i = 0; i < unique_tuple_.Size(); i++) {
+      auto tmp_result = unique_tuple_.template Get<GenericConfig<Ch>>(i)->Transform(document);
+      if (tmp_result.Success()) {
+        one_of_index_ = i;
+        n_matches++;
+      }
+      result.Append(tmp_result);
+    }
+    if (n_matches == 0) {
+      one_of_index_ = INVALID_ONE_OF_INDEX;
+      Result failed_result =
+          FailResult(fmt::format("No type in OneOf matched for the following {} reasons:", unique_tuple_.Size()));
+      failed_result.Append(result);
+      return failed_result;
+    } else if (n_matches > 1) {
+      return FailResult("Multiple matches - ambiguous result.");
+    }
+    return Result();
+  }
+
+  template <typename Config>
+  using ConfigOf = typename Tuple::template ElementWithCondition<internal::ConfigHasSameType<Config>::template Condition>;
+
+  template <typename Config>
+  GenericOneOf<Ch, Configs...>& operator=(const Config& config) {
+    one_of_index_ = ConfigOf<Config>::Index;
+    GetVariant<Config>() = config;
+    return *this;
+  }
+
+  template <typename Config>
+  const Config& GetVariant() const {
+    return unique_tuple_.template GetIfCondition<internal::ConfigHasSameType<Config>::template Condition>();
+  }
+
+  template <typename Config>
+  Config& GetVariant() {
+    return unique_tuple_.template GetIfCondition<internal::ConfigHasSameType<Config>::template Condition>();
+  }
+
+  template <typename Config>
+  void SetVariant(const Config& config) {
+    unique_tuple_.template GetIfCondition<internal::ConfigHasSameType<Config>::template Condition>() = config;
+  }
+
+  template <typename Config>
+  void SelectVariant() {
+    one_of_index_ = unique_tuple_.template IndexOf<internal::ConfigHasSameType<Config>::template Condition>();
+  }
+
+  template <typename Config>
+  bool Is() {
+    return ConfigOf<Config>::Index == one_of_index_;
+  }
+
+  Result Validate() const override {
+    assert(one_of_index_ != INVALID_ONE_OF_INDEX);
+    return unique_tuple_.template Get<Config>(one_of_index_)->Validate();
+  }
+
+  void Serialize(AbstractWriter<Ch>* writer) const override {
+    return unique_tuple_.template Get<Config>(one_of_index_)->Serialize(writer);
+  }
+
+  Result HandleMissing() const override {
+    return FailResult("is missing");
+  }
+
+ private:
+  int32_t one_of_index_ = INVALID_ONE_OF_INDEX;
+  // must be a tuple because each config and its constraints must be stored
+  Tuple unique_tuple_;
+
+  explicit GenericOneOf(Tuple&& data)
+      : unique_tuple_(std::forward<Tuple>(data)) {}
+
+  friend GenericOneOf<Ch, Configs...> MakeOneOf<Configs...>(Configs&&... configs);
+};
+
+template <typename... Configs>
+using OneOf = GenericOneOf<char, Configs...>;
+
+template<typename... Configs> RAPIDSCHEMA_REQUIRES(UniqueTypes<Configs...>)
+OneOf<Configs...> MakeOneOf(Configs&&... configs) {
+  return OneOf<Configs...>(
+      internal::UniqueTuple<Configs...>(
+          std::make_tuple<Configs...>(std::forward<Configs>(configs)...)));
+}
+
+}  // namespace rapidschema
+
+#endif  // INCLUDE_RAPIDSCHEMA_VARIANT_H_
---
+++ unittests/rapidschema/one_of_test.cpp
@@ -0,0 +1,100 @@
+// Copyright (C) 2019 Christian Ledergerber
+
+#include <tuple>
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+#include "rapidschema/no_additional_properties.h"
+#include "rapidschema/object.h"
+#include "rapidschema/one_of.h"
+#include "rapidschema/range_constraints.h"
+#include "rapidschema/test_utils.h"
+#include "rapidschema/transform_result_matchers.h"
+#include "rapidschema/value.h"
+
+namespace rapidschema {
+
+using testing::Test;
+
+class OneOfIntegerTestObject : public Object {
+ public:
+  OneOfIntegerTestObject()
+      : integer_value(MakeValue(22, Minimum<int>(100))) {}
+
+  Value<int, Minimum> integer_value;
+
+ protected:
+  PropertyMapping CreatePropertyMapping() const override {
+    return {{"integerValue", &integer_value}};
+  }
+};
+
+class OneOfStringTestObject : public Object {
+ public:
+  OneOfStringTestObject()
+      : string_value(MakeValue<std::string>("hallo")) {}
+
+  Value<std::string> string_value;
+
+ protected:
+  PropertyMapping CreatePropertyMapping() const override {
+    return {{"stringValue", &string_value}};
+  }
+};
+
+class OneOfTest : public Test {
+ public:
+  OneOf<OneOfIntegerTestObject, OneOfStringTestObject> one_of_;
+};
+
+/////////////////////////// Parse DOM Style /////////////////////////////////////////////
+
+TEST_F(OneOfTest, GivenSingleVariantMatches_WhenParsingDom_ThenCorrectlyParsed) {
+  auto result = ParseConfig(R"({"integerValue":12})", &one_of_);
+  ASSERT_TRUE(result.Success());
+  ASSERT_EQ(12, one_of_.GetVariant<OneOfIntegerTestObject>().integer_value.Get());
+  ASSERT_TRUE(one_of_.Is<OneOfIntegerTestObject>());
+}
+
+TEST_F(OneOfTest, GivenNoVariantMatches_WhenParsingDom_ThenCorrectlyErrorMessages) {
+  auto result = ParseConfig(R"({"integerValue":"string"})", &one_of_);
+  ASSERT_EQ(3, result.GetFailures().size());
+  ASSERT_EQ("No type in OneOf matched for the following 2 reasons:", result.GetFailures()[0].GetMessage());
+  ASSERT_EQ("Expected type: int. Actual value was: \"string\"", result.GetFailures()[1].GetMessage());
+  ASSERT_EQ("Value of type string is missing", result.GetFailures()[2].GetMessage());
+}
+
+TEST_F(OneOfTest, GivenMultipleVariantMatches_WhenParsingDom_ThenCorrectlyErrorMessages) {
+  OneOf<OneOfIntegerTestObject, NoAdditionalProperties<OneOfIntegerTestObject>> example;
+  auto result = ParseConfig(R"({"integerValue":12})", &example);
+  ASSERT_THAT(result, TransformFailed("Multiple matches - ambiguous result.", ""));
+}
+
+/////////////////////////// Validate /////////////////////////////////////////////
+
+TEST_F(OneOfTest, GivenOutOfRangeValue_WhenValidating_ThenCorrectlyErrorMessage) {
+  one_of_.SelectVariant<OneOfIntegerTestObject>();
+  auto result = one_of_.Validate();
+  ASSERT_THAT(result, TransformFailed("Expected: >= 100. Actual: 22", "integerValue"));
+}
+
+TEST_F(OneOfTest, GivenInRangeValue_WhenValidating_ThenSuccess) {
+  one_of_.GetVariant<OneOfIntegerTestObject>().integer_value = 200;
+  one_of_.SelectVariant<OneOfIntegerTestObject>();
+  auto result = one_of_.Validate();
+  ASSERT_TRUE(result.Success());
+}
+
+/////////////////////////// Serialization /////////////////////////////////////////////
+
+TEST_F(OneOfTest, WhenSerialized_ThenCorrectVariantSerialized) {
+  one_of_.SelectVariant<OneOfIntegerTestObject>();
+  std::string result = SerializeConfig(one_of_);
+  ASSERT_EQ(R"({"integerValue":22})", result);
+  one_of_.SelectVariant<OneOfStringTestObject>();
+  result = SerializeConfig(one_of_);
+  ASSERT_EQ(R"({"stringValue":"hallo"})", result);
+}
+
+}  // namespace rapidschema
